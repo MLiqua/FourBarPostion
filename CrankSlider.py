39cc51ba8d0ed6e0004abee2538a9d7e20359696
Matthew R. Liqua <Mliqua@tutanota.com>
2025-03-01 23:44:20 -0500

    Add files via upload

--- CrankSlider.py
+++ CrankSlider.py
@@ -1,7 +1,8 @@
 ###############################
 '''
 # Author: Matthew Ryan Liqua
-# Date:2/19/25
+# Created Date: 2/22/25
+# Last Edited Date: 3/1/25
 # Course: ENGR Mech Design
 # Purpose: Calcuates postion, velocity, and accleration
 			of crank slider mechanism
@@ -11,32 +12,39 @@
 from math import sin, cos, tan, acos, asin, atan, pi, degrees, radians
 from cmath import sqrt
 
-L1 = int(input("Input L1 (mm): "))
-L2 = int(input("Input L2 (mm): "))
-L3 = int(input("Input L3 (mm): "))
-# L4 = int(input("Input L4 (mm): "))  
-
-theta1 = radians(int(input("Input theta 1 (degree): ")))
-theta2 = radians(int(input("Input theta 2 (degree): ")))
-#theta3 = radians(int(input("Input theta 3 (degree): ")))
-theta4 = radians(int(input("Input theta 4 (degree): "))) 
+print("\n")
+print("This calculation assumes ground is d [R1] and theta4 = 90")
+print("Recall that any -d [R1] value means it is an non-real circuit")
+print("Ensure that all units for length are the same.")
+a = float(input("Input a [R2] length: "))
+b = float(input("Input b [R3] length: "))
+c = float(input("Input c [R4] length: "))
+theta2 = radians(float(input("Input theta 2 (degree): ")))
 print("\n")
 
 ####################################################
 # First theta 3 value
 
-OutTheta3_1 = asin( ((L1 * sin(theta2) - L3) / L2) )
-print("First theta 3 value is: ", round(degrees(OutTheta3_1),2))
+OutTheta3_1 = asin( ((a * sin(theta2) - c) / b) )
+print("Theta 3_1 value is: ", round(degrees(OutTheta3_1),2))
 
-OutD_1 = L1*cos(theta2) - L2*cos(OutTheta3_1)
-print("First L4 ('d') value in mm:", round(OutD_1,2))
+OutD_1 = a*cos(theta2) - b*cos(OutTheta3_1)
+print("d_1 [R1] value is:", round(OutD_1, 2))
+if OutD_1 > 0:
+	print("This is the real circuit.")
+else:
+	print("This is the non-real circuit.")
 print("\n")
 
 ####################################################
 # Second theta 3 value
 # asin is a multi valued function, hence OutTheta3_2
-OutTheta3_2 = asin( - ((L1 * sin(theta2) - L3) / L2) ) + pi
-print("Second theta 3 value is: ", round(degrees(OutTheta3_2),2 ))
+OutTheta3_2 = asin( - ((a * sin(theta2) - c) / b) ) + pi
+print("Theta 3_2 value is: ", round(degrees(OutTheta3_2),2 ))
 
-OutD_2 = L1*cos(theta2) - L2*cos(OutTheta3_2)
-print("Second L4 ('d') value in mm: ", round(OutD_2,2))
+OutD_2 = a*cos(theta2) - b*cos(OutTheta3_2)
+print("d_2 [R1] value is: ", round(OutD_2,2))
+if OutD_2 > 0:
+	print("This is the real circuit.")
+else:
+	print("This is the non-real circuit.")
